e9af7216dc3042fe04d70d847cc738fea1b0699e
Hillarie-Kip <kalyahillary@gmail.com>
2023-06-29 15:26:52 +0300

    clean up

--- Program.cs
+++ Program.cs
@@ -1,21 +1,28 @@
 ﻿using System;
+using System.Globalization;
+using System.Timers;
+using Timer = System.Timers.Timer;
 
 public class Program
 {
     public static string GenerateSaleId(DateTime timestamp)
     {
-        char year = (char)('A' + (timestamp.Year - 2004));
+        char year = (char)('A' + (timestamp.Year - 2006));
         char month = (char)('A' + (timestamp.Month - 1));
         string day = ConvertToLetterNumberCombinations(timestamp.Day);
-        string hour = timestamp.ToString("HH");
+        string hour = ConvertToLetterNumberCombinations(timestamp.Hour);
+       // string hour = timestamp.ToString("HH");
         string minutes = ConvertToLetterCombinations(timestamp.Minute);
         string seconds = ConvertToLetterCombinations(timestamp.Second);
-        // string millisecond = ConvertToLetterCombinations(timestamp.Millisecond);
-        string millisecond = timestamp.Millisecond.ToString();
+         string millisecond = ConvertToLetterCombinationsMillisec(timestamp.Millisecond);
+        // string millisecond = timestamp.Millisecond.ToString();
+        string userID = ConvertToLetterCombinationsUserID(10);
+        string venodrLocationID = ConvertToLetterCombinationsUserID(10);
 
 
 
-        string saleId = $"{year}-{month}-{day}-{hour}-{minutes}-{seconds}";
+        // string saleId = $"{year}-{month}-{day}-{hour}-{minutes}-{seconds}-{millisecond}";
+        string saleId = $"{year}{month}{day}{hour}{minutes}{seconds}{millisecond}-{userID}";
 
         return saleId;
     }
@@ -32,7 +39,88 @@ public class Program
         return romanNumerals[number - 1];
     }
 
+
     public static string ConvertToLetterCombinations(int number)
+    {
+        if (number < 0 || number > 59)
+        {
+            throw new ArgumentOutOfRangeException(nameof(number), "Invalid minutes/seconds value");
+        }
+
+        string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        string result = "";
+
+        while (number > 0)
+        {
+            number--;
+            result = letters[number % 26] + result;
+            number /= 26;
+        }
+
+        return result.PadLeft(2, 'A'); // Pad with leading zeros if needed
+    }
+
+    public static string ConvertToLetterCombinationsMillisec(int number)
+    {
+        if (number < 0 || number > 1000)
+        {
+            throw new ArgumentOutOfRangeException(nameof(number), "Invalid minutes/seconds value");
+        }
+
+        string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890";
+        string result = "";
+
+        while (number > 0)
+        {
+            number--;
+            result = letters[number % 36] + result;
+            number /= 36;
+        }
+
+        return result;
+    }
+
+
+    public static string ConvertToLetterCombinationsUserID(int number)
+    {
+        if (number < 0 || number > 10000000)
+        {
+            throw new ArgumentOutOfRangeException(nameof(number), "Invalid minutes/seconds value");
+        }
+
+        string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890";
+        string result = "";
+
+        while (number > 0)
+        {
+            number--;
+            result = letters[number % 36] + result;
+            number /= 36;
+        }
+
+        return result;
+    }
+
+    public static int ConvertToNumber(string letter)
+{
+    if (string.IsNullOrEmpty(letter))
+    {
+        throw new ArgumentException("Letter cannot be null or empty", nameof(letter));
+    }
+
+    string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890";
+    letter = letter.ToUpper();
+
+    int number = 0;
+    for (int i = 0; i < letter.Length; i++)
+    {
+        number = (number * 36) + letters.IndexOf(letter[i]);
+    }
+
+    return number + 1;
+}
+
+    public static string ConvertToLetterCombination(int number)
     {
         if (number < 0 || number > 59)
         {
@@ -73,14 +161,30 @@ public class Program
 
     public static void Main(string[] args)
     {
-      
+        Timer timer = new Timer(1000); // 1 millisecond interval
+        timer.Elapsed += TimerElapsed;
+        timer.AutoReset = true;
+        timer.Start();
+
+        Console.WriteLine("Press Enter to stop the program.");
+        Console.ReadLine();
+
+        timer.Stop();
+        timer.Dispose();
+
+    }
+
+
+    static void TimerElapsed(object sender, ElapsedEventArgs e)
+    {
+        // Code to be executed every millisecond
         string saleId = GenerateSaleId(DateTime.Now);
 
-        Console.WriteLine($"Sale ID: {saleId}");
+        Console.WriteLine($"Sale ID:{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff", CultureInfo.InvariantCulture)} -- {saleId} ");
 
 
-        Console.WriteLine($"Sale ID: {DateTime.Now.Ticks.ToString().Substring(0, 10)}");
+       // Console.WriteLine($"Sale ID: {DateTime.Now.Ticks.ToString().Substring(0, 10)}");
 
-        
+
     }
 }
